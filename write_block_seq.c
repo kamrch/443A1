d9fc7de5cae4bd2269c8a02caffc3852d6217d6a
kamrch <ryan921ryan@hotmail.com>
2017-01-27 13:24:37 -0500

    Added 2 files

---
+++ write_block_seq.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+typedef struct record {
+ int uid1;
+ int uid2;
+} Record;
+
+int main(int argc, char *argv[]){
+		/*Check if user input correct arguments*/
+		if (argc != 3){
+    		printf("Usage: write_blocks_seq <input filename> <block size>\n");
+    		return -1;
+		}
+		char *file_name = argv[1];
+		int *block_size = argv[2];		
+		
+}
