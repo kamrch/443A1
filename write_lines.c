00f52e78bf44f80cb0494fe20353622ccde4b340
Ryan <ryan921ryan@hotmail.com>
2017-01-31 13:50:04 -0500

    updated write_line

--- write_lines.c
+++ write_lines.c
@@ -12,14 +12,14 @@ int main(int argc, char *argv[]){
         return -1;
     }
     char *file_name = argv[1];
-
+    char *line;
     char current_line[MAX_CHARS_PER_LINE];
 
     //int records_per_block = 1;
 
     /*Codes for recording time*/
     struct timeb t_begin, t_end;
-    long total_records = 0;
+    long total_length = 0;
 
     long time_spent_ms;
 
@@ -40,26 +40,9 @@ int main(int argc, char *argv[]){
     /*Start clock before or after allocating buffer?*/
 
     /*Loop for writing to file*/
-    while(fgets (current_line, MAX_CHARS_PER_LINE, fp_read)!=NULL ) {
-        current_line [strcspn (current_line, "\r\n")] = '\0'; //remove end-of-line characters
-        if (strlen(current_line) > 0){
-            Record r = parseRecord(current_line);
-            if (offset >= records_per_block){
-                fwrite ( buffer, sizeof(Record), offset, fp_write);
-                fflush (fp_write);
-                n = 0;
-                offset = 0;
-            }
-            buffer[n] = r;
-            n+=1;
-            offset+=1;
-            total_records += 1;
-        }
-    }
-
-    /*Write remaining data to file, if any*/
-    if (offset > 0){
-        fwrite(buffer, sizeof(Record), offset, fp_write);
+    while((line=fgets (current_line, MAX_CHARS_PER_LINE, fp_read))!=NULL ) {
+        fputs(line, fp_write);
+        total_length += strlen(current_line);
     }
 
     fflush(fp_write);
@@ -75,6 +58,6 @@ int main(int argc, char *argv[]){
 
 
     /* result in MB per second */
-    printf ("Data rate: %.3f MBPS\n", ((total_records*sizeof(Record))/(float)time_spent_ms * 1000)/(1024*1024));
+    printf ("Data rate: %.3f MBPS\n", (total_length/(float)time_spent_ms * 1000)/(1024*1024));
 
 }
