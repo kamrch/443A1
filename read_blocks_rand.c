3828d9f3e77312c0801ba7c4f32ce58dbe40063f
Ryan <ryan921ryan@hotmail.com>
2017-01-31 19:56:45 -0500

    small fix

--- read_blocks_rand.c
+++ read_blocks_rand.c
@@ -7,12 +7,12 @@
 int main(int argc, char *argv[]){
 	    /*Check if user input correct arguments*/
     if (argc != 4){
-        printf("Usage: read_blocks_rand <input filename> <block_size> <random_number\n");
+        printf("Usage: read_blocks_rand <input filename> <block_size> <random_number>\n");
         return -1;
     }
     char *file_name = argv[1];
-    int block_size = atoi(atgv[2]);
-		int rand_num = atoi(atgv[3]);
+    int block_size = atoi(argv[2]);
+		int random_number = atoi(argv[3]);
     
 		int records_per_block = block_size / sizeof(Record);	
 		
@@ -30,6 +30,7 @@ int main(int argc, char *argv[]){
 		int average = 0;
 		struct timeb t_begin, t_end;
 		
+		/*Use fseek to find the number of records*/
 		fseek(fp_read, 0L, SEEK_END);
 		int file_size = ftell(fp_read);
 		fseek(fp_read, 0L, SEEK_SET);
