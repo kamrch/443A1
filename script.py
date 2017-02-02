975e2b3119bec9f98d034db99148a4e248afa572
kamrch <ryan921ryan@hotmail.com>
2017-02-01 19:03:26 -0500

    done script

--- script.py
+++ script.py
@@ -1,5 +1,5 @@
 import os
-import subprocess
+import sys
 
 KB = 1024
 MB = 1024 * 1024
@@ -17,21 +17,20 @@ sizes = [
 
 filenames = ["edges.csv", "edges_1.csv",  "edges_2.csv",  "edges_3.csv",  "edges_4.csv",  "edges_5.csv"]
 
-for i in range(0, len(filenames)):
-    fBlocks = open("block_result_"+ str(i+1) + ".txt", "w")
-    
-    
-    for size in sizes:
-        #Use subprocess to execute commands
-        subprocess.run(["write_blocks_seq", filenames[i], str(size)], stdout=fBlocks)
-        subprocess.run(["rm", "records.dat"])        
 
-    fLines = open("line_result_"+ str(i+1) + ".txt", "w")
-    
-    subprocess.run(["write_lines", filename[i]], stdout=fLines)
-    subprocess.run(["rm", "records.dat"])
-    
-    fLines.close()
-    fBlocks.close()
-    
-    print ("*************Iteration number " + str(i+1)+"************")
+#loop for write_lines
+for k in range(0,len(filenames)):
+        os.system("echo '======Iteration "+str(k+1)+"======' >> line_result.txt"); 
+        os.system("./write_lines "+filenames[k]+" >> line_result.txt"); 
+        os.system("rm records.dat")
+
+
+#Loop for write_blocks_seq with different block size
+for size in sizes:
+    for j in range(0,len(filenames)):
+        os.system("echo '======Iteration "+str(j+1)+"======' >> blocks_seq_"+str(size)+".txt"); 
+        os.system("./write_blocks_seq "+filenames[j]+" " + str(size) + " >> blocks_seq_"+str(size)+".txt");
+        os.system("rm records.dat") 
+
+
+print("Script completed.")
